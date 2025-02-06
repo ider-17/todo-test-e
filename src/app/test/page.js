baea13f85d1cfdd6aea1f85f5c2b54be1a688453
24LP6042 24LP6042 <24LP6042@G303-CC0142.local>
2025-02-06 13:08:41 +0800

    updated

--- src/app/test/page.js
+++ src/app/test/page.js
@@ -2,20 +2,60 @@
 
 import { useState } from "react";
 
-export default function Home() {
+const initialTasks = [];
 
-    const [tasks, setTasks] = useState("")
+export default function Home() {
+    const [firstName, setFirstName] = useState('')
+    const [tasks, setTasks] = useState(initialTasks);
 
     function addTask() {
-        const addNewTask = prompt("Task?");
-        const addTasks = [{text: addNewTask, isCompleted: false, id: "1"}];
-        
+        const addTask = firstName
+        if (addTask) {
+            const addTaskList = [{ task: addTask, completed: false, id: new Date().toISOString() }, ...tasks]
+            setTasks(addTaskList)
+        }
+        setFirstName("")
+    }
 
-    };
+    function editTask(index) {
+        const editTask = prompt("Edit Task?", tasks[index].task)
+        if (editTask) {
+            const clonedTask = [...tasks]
+            clonedTask[index].task = editTask
+            setTasks(clonedTask)
+        }
+    }
+
+    function deleteTask(id) {
+        if (confirm("Are You Sure?")) {
+            const clonedTask = tasks.filter((task) => task.id !== id)
+            setTasks(clonedTask)
+        }
+    }
+
+    function toggleCompleted(index) {
+        const clonedTask = [...tasks]
+        clonedTask[index].completed = !clonedTask[index].completed
+        setTasks(clonedTask)
+    }
+
+    function handleKeyDown(e) {
+        if (e.code === "Enter") {
+            addTask()
+        }
+    }
 
     return (
         <div>
+            <input type="text" onKeyDown={handleKeyDown} value={firstName} onChange={e => setFirstName(e.target.value)} />
             <button onClick={addTask}>Add</button>
+            {tasks.map((task, index) =>
+                <div key={task.id} className="task">
+                    <input type="checkbox" checked={task.completed} onChange={() => toggleCompleted(index)} />
+                    <span className={task.completed ? "checkbox" : ''}>{task.task}</span>
+                    <button onClick={() => editTask(index)}>Edit</button>
+                    <button onClick={() => deleteTask(task.id)}>Delete</button>
+                </div>)}
         </div>
     )
 }
